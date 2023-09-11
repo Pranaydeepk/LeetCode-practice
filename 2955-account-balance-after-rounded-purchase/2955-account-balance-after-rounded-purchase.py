6aa21980aa61561210751be83c5ed8ec1e64e38a
Pranaydeepk <124602365+Pranaydeepk@users.noreply.github.com>
2023-09-11 22:26:56 +0530

    Time: 47 ms (7.43%), Space: 16.3 MB (58.71%) - LeetHub

--- 2955-account-balance-after-rounded-purchase/2955-account-balance-after-rounded-purchase.py
+++ 2955-account-balance-after-rounded-purchase/2955-account-balance-after-rounded-purchase.py
@@ -1,14 +1,3 @@
 class Solution:
     def accountBalanceAfterPurchase(self, purchaseAmount: int) -> int:
-        deduction = 0
-        if purchaseAmount <= 10:
-            if purchaseAmount < 5:
-                deduction = 0
-            else:
-                deduction = 10
-        else:
-            if purchaseAmount < (purchaseAmount//10)*10+5:
-                deduction = (purchaseAmount//10)*10
-            else:
-                deduction = (purchaseAmount//10)*10+10
-        return 100 - deduction
+        return 100-((purchaseAmount+5)//10)*10
