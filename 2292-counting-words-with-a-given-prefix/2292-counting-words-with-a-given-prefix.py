8aef3e2fc30772c32856474a9d99799bbfd0de79
Pranaydeepk <124602365+Pranaydeepk@users.noreply.github.com>
2023-09-13 22:24:54 +0530

    Time: 54 ms (32.36%), Space: 16.3 MB (80.6%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.py
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.py
@@ -3,14 +3,12 @@ class Solution:
         count = 0
         prefix_len = len(pref)
         for word in words:
-            word_len = len(word)
-            if word_len < prefix_len:
-                continue
-            is_prefix = True
-            for i in range(prefix_len):
-                if word[i] != pref[i]:
-                    is_prefix = False
-                    break
-            if is_prefix:
-                count += 1
+            if len(word) >= prefix_len:
+                is_prefix = True
+                for i in range(prefix_len):
+                    if word[i] != pref[i]:
+                        is_prefix = False
+                        break
+                if is_prefix:
+                    count += 1
         return count
