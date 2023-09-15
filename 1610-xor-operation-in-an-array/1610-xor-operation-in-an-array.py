a2c19bf1009ad429dd6cf768751194b9d772964d
Pranaydeepk <124602365+Pranaydeepk@users.noreply.github.com>
2023-09-15 09:58:36 +0530

    Time: 42 ms (38.83%), Space: 16.2 MB (68.08%) - LeetHub

---
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        nums = [(start + 2*i) for i in range(n)]
+        result = nums[0]
+        for num in nums[1:]:
+            result ^= num
+        return result
